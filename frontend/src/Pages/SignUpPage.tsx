0169d08817b880355998c04fcc382fc6deb525c6
pleum-attakorn <68465085+pleum-attakorn@users.noreply.github.com>
2020-10-11 12:35:47 +0700

    Add files via upload
    SignUppage tsx modify1.0

--- frontend/src/Pages/SignUpPage.tsx
+++ frontend/src/Pages/SignUpPage.tsx
@@ -5,14 +5,71 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import '../CSSsource/LoginSigninPage.css';
+import '../CSSsource/SignupPage.css';
 
 
 const SignUpPage = () => {
-  
 
   return (
     <Router>
+      <div className="dell-x-p-s">
+        <div className="center-frame">
+            <div className="k-u-people">
+                  KU people
+            </div>
+            <div className="descb-text">
+                  Please sign up or log in before use. 
+            </div>
+            <div className="accountName">
+              <form>
+                  Account Name :
+                  <input placeholder="Type your username... (Only characters and Numbers allowed.)" style={{ width:"480px" , height:"50px" }} onFocus= {undefined} onBlur={undefined} onChange={undefined} onSubmit={undefined} value={""}  className="Input_account"/>
+                 
+              </form>
+            </div>
+            <div className="email-text">
+              <form>
+                  Email :
+                  <input placeholder="Type your Email..." style={{ width:"480px" , height:"50px" }} onFocus= {undefined} onBlur={undefined} onChange={undefined} onSubmit={undefined} value={""}  className="Input_email"/>
+                  <div className="_ku-th">
+                    @ku.th
+                  </div>
+              </form>
+            </div>
+            <div className="password">
+              <form>
+                  Password :
+                  <input placeholder="Type your password..." style={{ width:"480px" , height:"50px" }} onFocus= {undefined} onBlur={undefined} onChange={undefined} onSubmit={undefined} value={""}  className="Input_password"/>
+                 
+              </form>
+            </div>
+            <div className="conpassword">
+              <form>                  
+                  Confirm Password :
+                  
+                  <input placeholder="Comfirm your password..." style={{ width:"480px" , height:"50px" }} onFocus= {undefined} onBlur={undefined} onChange={undefined} onSubmit={undefined} value={""}  className="Input_conpassword"/>
+                 
+              </form>
+            </div>
+            <div className="accept">
+                  You alreay read and accept
+            </div>
+            <div className="term">
+                  Term of use Agreement
+            </div>
+            <div className="_go-back">
+              <button className="button">
+                  &lt; Go Back
+              </button>
+            </div>
+            <div className="sign-up">
+              <button className="button2">
+                <button className="square"></button>
+                  &lt; Sign Up
+              </button>
+            </div>
+        </div>
+      </div>
       
     </Router>
   );
