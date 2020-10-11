11430a15350092c1ec538d75942d2ca875f9b623
pleum-attakorn <68465085+pleum-attakorn@users.noreply.github.com>
2020-10-11 15:48:09 +0700

    Add files via upload
    ForgetPwd tsx 11/10 15:46

--- frontend/src/Pages/ForgetPwd.tsx
+++ frontend/src/Pages/ForgetPwd.tsx
@@ -5,15 +5,66 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
+import '../CSSsource/ForgetPwd.css';
 
 const ForgetPwd = () => {
   
 
   return (
-      <div>
-          ควย
-      </div>
+      <Router>
+        <div className="forgetpwd-frame">
+            <div className="forgetpwd-whiteframe">
+                <div className="forgetpwd-k-u-people">
+                    KU People
+                </div>
+                <div className="password-reset">
+                    Password reset
+                </div>                
+                <div className="forgetpwd-email_">
+                <form>
+                    E-mail :
+                    <input placeholder="Type your Email..." style={{ width:"480px" , height:"50px" }} onFocus= {undefined} onBlur={undefined} onChange={undefined} onSubmit={undefined} value={""}  className="forgetpwd_email_input"/>                    
+                </form>
+                </div>
+                <div className="send">
+                    <button className="send_button">                        
+                        send
+                    </button>
+                </div>
+                <div className="verification">
+                    verification code will send to you. It expired after 5 minutes.
+                </div>                
+                <div className="new-password_">
+                <form>
+                    New Password :
+                    <input placeholder="Type your password..." style={{ width:"480px" , height:"50px" }} onFocus= {undefined} onBlur={undefined} onChange={undefined} onSubmit={undefined} value={""}  className="newpassword_input"/>                    
+                </form>
+                </div>
+                <div className="confirm-password_">
+                <form>
+                    Confirm Password :
+                    <input placeholder="Type your password..." style={{ width:"420px" , height:"50px" }} onFocus= {undefined} onBlur={undefined} onChange={undefined} onSubmit={undefined} value={""}  className="confirmpassword_input"/>                    
+                </form>
+                </div>
+                <div className="verification-code_">
+                <form>
+                    Verification Code :
+                    <input placeholder="Type your verification code..." style={{ width:"440px" , height:"50px" }} onFocus= {undefined} onBlur={undefined} onChange={undefined} onSubmit={undefined} value={""}  className="verification-code_input"/>                    
+                </form>
+                </div>
+                <div className="fgpwd_go-back">
+                    <button className="fgpwd_goback_button">
+                        &lt; Go Back
+                    </button>
+                </div>
+                <div className="fgpwd_confirm">
+                    <button className="fgpwd_confirm_button">                        
+                        Confirm
+                    </button>
+                </div>
+            </div>
+        </div>
+      </Router>
   );
 }
 
