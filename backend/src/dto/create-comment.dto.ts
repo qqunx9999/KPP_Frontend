8afd7dff3ebe34bf44af3d45a4e9d2a3f590cccc
sansern <sansern.k@ku.th>
2020-10-13 21:27:38 +0700

    create comment dto alpha

---
+++ backend/src/dto/create-comment.dto.ts
@@ -0,0 +1,27 @@
+import { IsInt , IsNotEmpty , IsBoolean , IsDate , IsString } from "class-validator";
+import {ObjectID} from 'mongodb';
+
+
+
+export class Commentation {
+    
+    userID:ObjectID;
+    
+    threadID:ObjectID;
+    @IsString()
+    content:string;
+    
+    image_arr:[string, number][];
+    @IsInt()
+    reply_to:number;
+    @IsInt()
+    commentNO:number;
+    @IsDate()
+    date_create:Date;
+    @IsDate()
+    date_lastedit:Date;
+    @IsDate()
+    date_delete:Date;
+    @IsBoolean()
+    isanonymous:boolean;
+}
