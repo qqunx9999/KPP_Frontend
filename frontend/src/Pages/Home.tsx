d5de486a655321cab3ce430acd4810a37c3f41b2
ppjaisri <ppjaisri@gmail.com>
2020-10-13 13:10:04 +0700

    Home prototype

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -5,12 +5,46 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { Thread } from '../interfaces/newsEntity';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Home = () => {
+  const [thread, setThread] = useState<Thread[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000')
+      .then(res => res.json())
+      .then(obj => {
+        setThread(obj.json());
+      });
+  },[]);
 
   return (
-     <div>
-       
+     <div className="container">
+       <h6>Latest</h6>
+       <ul>
+         <Link to={ `Threads` }>
+           { thread.map(item => {
+             return <li key={ item.userID }>{ item.content }</li>
+           }) }
+         </Link>
+       </ul>
+       <h6>Hottest</h6>
+       <ul>
+         <Link to={ `Threads` }>
+          { thread.map(item => {
+              return <li key={ item.userID }>{ item.content }</li>
+            }) }
+         </Link>
+       </ul>
+       <h6>News</h6>
+       <ul>
+        <Link to={ `Threads` }>
+          { thread.map(item => {
+            return <li key={ item.userID }>{ item.content }</li>
+          }) }
+        </Link>
+       </ul>
      </div>
   );
 }
