fdcf6b0ce4ee6fc2b9a2b5882405d213c4c9f701
BLAXList <blaxlist2017@gmail.com>
2020-10-15 14:35:56 +0700

    CreateSettingPage

--- frontend/src/Pages/Setting.tsx
+++ frontend/src/Pages/Setting.tsx
@@ -5,13 +5,60 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
+import '../CSSsource/Setting.css';
 const Setting = () => {
   
 
   return (
       <div>
+        <div className="backgroundUserSetting">
+          <div className="frameWhiteUserSetting">
+            <div className="textSettingUserSetting">
+              Setting
+            </div>
+            <div className="frameBlackUserSetting">
+              <div className="textNameUserProfile">
+                Name :
+              </div>
+              <div className="nameUserProfile">
+                PatrickStar
+              </div>
+              
+              <div className="textUsernameUserProfile">
+                Username :
+              </div>
+              <div className="usernameUserProfile">
+                Earth.p
+              </div>
 
+              <div className="textEmailUserProfile">
+                Email :
+              </div>
+              <div className="emailUserProfile">
+                Patrick.s@ku.th
+              </div>
+
+              <div className="textYourquoteUserProfile">
+                Your Quote :
+              </div>
+              <div className="userquoteUserProfile">
+                GUSHOPSHARKWOW!!!
+              </div>
+            </div>
+
+            <div className="frameGoBackUserProfile">
+              <div className="textGoBackUserProfile">
+                &lt; Go Back
+              </div>
+            </div>
+
+            <div className="frameChangePasswordUserProfile">
+              <div className="textChangePasswordUserProfile">
+                Change Password
+              </div>  
+            </div>
+          </div>
+        </div>
       </div>
   );
 }
