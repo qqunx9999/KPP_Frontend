a8a9ba5e1976fc40ba2704234e567c1cf3eb865d
BLAXList <blaxlist2017@gmail.com>
2020-10-16 17:06:08 +0700

    AddNavBarToCreateThreadPage**DropdownNotFunctional

--- frontend/src/Pages/SearchThread.tsx
+++ frontend/src/Pages/SearchThread.tsx
@@ -13,47 +13,47 @@ const SearchThread = () => {
 
   return (
     <div>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link to={ `/Home` }><a className="navbar-brand" href="#">KU People</a></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Link to={ `/Home` }><a className="navbar-brand" href="#">KU People</a></Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <Link to={ `/SearchThread` }><a className="nav-link" href="#">Seacrh Thread</a></Link>
-            </li>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link to={ `/SearchThread` }><a className="nav-link" href="#">Seacrh Thread</a></Link>
+              </li>
 
-            <li className="nav-item active">
-              <Link to={ `/CreateThread` }><a className="nav-link" href="#">Create Thread</a></Link>
-            </li>
-            
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">By KUTheWeeb</a>
-            </li>
+              <li className="nav-item active">
+                <Link to={ `/CreateThread` }><a className="nav-link" href="#">Create Thread</a></Link>
+              </li>
+              
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#">By KUTheWeeb</a>
+              </li>
 
-            <li className="nav-item active">
-              <a className="nav-link " href="#">
-                  ProfilePicHere
-                
-              </a>
-            </li>
+              <li className="nav-item active">
+                <a className="nav-link " href="#">
+                    ProfilePicHere
+                  
+                </a>
+              </li>
 
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="button">
-                Dropdown link
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="button">
+                  Dropdown link
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
 
-          </ul>
-        </div>
-      </nav>
+            </ul>
+          </div>
+        </nav>
 
 
 
