6cab82d022d9af6d9eb8be4205d23d81419c5c78
pleum-attakorn <Attakorm.su@ku.th>
2020-10-23 20:51:59 +0700

    new CreateComment - upload tsx and css

--- frontend/src/Pages/CreateComment_new.tsx
+++ frontend/src/Pages/CreateComment_new.tsx
@@ -4,6 +4,7 @@ import { Thread } from '../interfaces/threadEntity';
 import ThreadService from '../service/ThreadService';
 import Navigtion from '../component/NavBar';
 import CommentForm from '../component/CommentForm';
+import '../CSSsource/CreateComment.css';
 
 const CreateComment_new = () => {
   const [thread, setThread] = useState<Thread[]>([]);
@@ -27,15 +28,28 @@ const CreateComment_new = () => {
 
   return (
     <div style={ temp }>
-      <h1>Give Comment</h1>
-      <button onClick={ history.goBack }>Go Back</button> <br />
-      Topic : 
-      { thread.map(item => {
-        if (item.threadID === { ThreadID }.ThreadID) {
-          return " " + item.topic
-        }
-      }) }
-      <CommentForm />
+      <Navigtion />
+        <div className="createcm-bigframe">        
+        <button className="createcm_goback_button" onClick={ history.goBack }>&lt; Go Back</button> <br />
+        <div className="createcm-whiteframe">
+          <div className="createcm-give-comment">
+            Give Comment
+          </div>
+          <div className="createcm-topicname-frame">
+            <div className="createcm-in-topic_">
+              In Topic :
+            </div>
+            <div className="createcm-topic-name">              
+              { thread.map(item => {
+                if (item.threadID === { ThreadID }.ThreadID) {
+                  return " " + item.topic
+                }
+              }) }
+            </div>
+          </div>
+          <CommentForm />
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/component/CommentForm.tsx
+++ frontend/src/component/CommentForm.tsx
@@ -13,16 +13,40 @@ const CommentForm = () => {
         >
             {({ isSubmitting }) => (
                 <Form>
-                    <h6>Reply to: </h6>
-                    <Field type="input" name="reply" placeholder="Comment numbers" /> <br />
-                    <p>*If you do not reply just leave it empty</p>
-                    <button><b>B</b></button>
-                    <button><i>I</i></button>
-                    <h6>Size :</h6>
-                    <Field type="input" name="size" /> <br />
-                    <h6>Place your comment :</h6>
-                    <Field type="input" name="comment" />
-                    <button disabled={ isSubmitting }>Send</button>
+                    <div className="createcm-repltyo-frame">
+                        <div className="createcm-replyto">Reply to:</div>
+                        <Field type="input" id="createcm_replyto_input" name="reply"  placeholder="Comment numbers"  style={{ width:"250px" , height:"70px" }} />
+                    </div>
+                    
+                    <p className="createcm-leave-empty">*If you do not reply just leave it empty</p>
+                    <div className="createcm-cm-frame">
+                        <div className="createcm-placecm">Place your comment :</div>
+                        <div className="createcm-green-frame">
+                            <div className="createcm-cha">
+                                <button className="createcm-bold-frame"><b>B</b></button>
+                            </div>
+                            <div className="createcm-cha">
+                                <button className="createcm-Italic-frame"><i>I</i></button>
+                            </div>
+                            <div className="createrp-fonts">
+                                <button className="createrp-fonts-frame">
+                                    Fonts
+                                </button>
+                            </div>  
+                            <div className="createcm-size-frame">  
+                                <div className="createcm-size">            
+                                    Size 
+                                </div> 
+                                <Field type="input" name="size" className="createcm_size_input" style={{ width:"60px" , height:"40px" }} />
+                            </div>                            
+                            <div className="createcm-send">
+                                <button className="createcm-send-frame">
+                                    Send
+                                </button>
+                            </div>
+                        </div>
+                        <Field type="input" name="comment" className="createrp_reason_input" style={{ width:"1140px" , height:"250px" }}/>
+                    </div>
                 </Form>
             )}
         </Formik>
