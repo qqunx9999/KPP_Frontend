10a6b8a53e2613b93c6ac77cf8b5d75a897aa137
pleum-attakorn <Attakorm.su@ku.th>
2020-10-23 16:06:16 +0700

    create report new - add css to tsx

--- frontend/src/Pages/CreateReport_new.tsx
+++ frontend/src/Pages/CreateReport_new.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router';
 import ReportForm from '../component/ReportForm';
+import '../CSSsource/CreateReport.css';
+import Navigtion from '../component/NavBar';
 
 const CreateReport_new = () => {
   const history = useHistory();
@@ -12,10 +14,17 @@ const CreateReport_new = () => {
 
   return (
     <div style={ temp }>
-      <h1>Report</h1>
-      <button onClick={ history.goBack }>Go Back</button>
-      <ReportForm />
-      <p>Your report will b send to administrator. We will give the consideration about your report within 2 - 3 days.</p>
+      <Navigtion />
+      <div className="createrp-bigframe">        
+        <button className="createrp_goback_button" onClick={ history.goBack }>&lt; Go Back</button>
+        <div className="createrp_whiteframe">
+          <div className="createrp-report">Report</div>
+          <ReportForm />
+          <p className="createrp-desc-text">
+            Your report will be send to administrator. We will give the consideration about your report within 2 - 3 days.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
