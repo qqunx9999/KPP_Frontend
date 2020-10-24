8a4fb8526076053d8936d61321f6e69f639417b0
pleum-attakorn <Attakorm.su@ku.th>
2020-10-24 22:24:26 +0700

    ChangeName - firsttime upload tsx and css

--- frontend/src/Pages/ChangeName.tsx
+++ frontend/src/Pages/ChangeName.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import '../CSSsource/ChangeName.css';
+import { useHistory, useParams } from 'react-router';
+import Navigtion from '../component/NavBar';
+import { Formik, Form, Field } from 'formik';
+import ChangeNameForm from '../component/ChangeNameForm';
 
 function ChangeName() {
 
+    const history = useHistory();
+    const temp = {
+      "margin": "10px",
+    };
 
     return (
-      <div >
-  
+      <div style={ temp }>
+        <Navigtion />
+        <div className="cName-bigframe">
+          <button className="cName_goback_button" onClick={ history.goBack }>&lt; Go Back</button>
+          <div className="cName-whiteframe">
+            <div className="cName-change-name">Change Name</div>
+            <div className="cName-blackframe">
+              <div className="cName-new-name_">New Name :</div>
+              <ChangeNameForm />
+            </div>
+            <button className="cName-confirmbutton" onClick={ history.goBack }>&lt; Confirm</button>
+
+          </div>
+        </div>  
   
       </div>
     );
