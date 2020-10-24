792d09957dd9e367bfc10d53aaeffc84b04f1406
pleum-attakorn <Attakorm.su@ku.th>
2020-10-24 14:50:46 +0700

    Authenlogout - first time upload

--- frontend/src/Pages/AuthenLogout.tsx
+++ frontend/src/Pages/AuthenLogout.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../CSSsource/AuthenLogout.css';
+import { Redirect } from 'react-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function AuthenLogout() {
+    const [time, setRedirect] = useState<boolean>(false);
 
+    const delay = () => {
+        setRedirect(true);
+      };
 
     return (
-      <div >
-  
-  
+      <div className="AtLogout_bigframe">
+          <div className="container-md">
+            <div className="AtLogout_whiteframe">
+                <div className="AtLogout_k-u-people">KU People</div>
+                <div className="rotate"></div>
+                <div className=" spinner-border text-success" id="AtLogout_spin" role="status">
+                    <span className="sr-only">Loading Out...</span>
+                    
+                </div>
+                <div className="AtLogout_success">Logout success! Go to home...</div>
+            </div>
+          </div>
+          {setInterval(delay, 3000)}
+          {time && (<Redirect to="/" />)}
       </div>
     );
   }
