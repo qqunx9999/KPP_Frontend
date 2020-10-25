b4788becf1f699a07ed3b96d1b0f13170dffa6c0
pchya <phitchaya.le@ku.th>
2020-10-25 17:02:26 +0700

    reports

--- backend/src/reports/reports.module.ts
+++ backend/src/reports/reports.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import Reportment_comment from 'src/entities/reportment_comment.entity';
+import Reportment_thread from 'src/entities/reportment_thread.entity';
+import Commentation from 'src/threads/comentation.entity';
+import Thread from 'src/threads/thread.entity';
+
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Thread, Commentation, Reportment_thread,Reportment_comment])],
   controllers: [ReportsController],
   providers: [ReportsService]
 })
+
 export class ReportsModule {}
--- backend/src/reports/reports.service.ts
+++ backend/src/reports/reports.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ObjectID } from 'mongodb';
+
+import Reportment_comment from 'src/entities/reportment_comment.entity';
+import Reportment_thread from 'src/entities/reportment_thread.entity';
+import Commentation from 'src/threads/comentation.entity';
+import Thread from 'src/threads/thread.entity';
+
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Thread)
+    private threadsRepository: Repository<Thread>,
+    @InjectRepository(Commentation)
+    private commentationsRepository: Repository<Commentation>,
+    @InjectRepository(Reportment_thread)
+    private reportment_threadsRepository: Repository<Reportment_thread>,
+    @InjectRepository(Reportment_comment)
+    private reportment_commentsRepository: Repository<Reportment_comment>
+      
+  ) {}
+
+  async findAllReportedTs(): Promise<Reportment_thread[]> {
+    return this.reportment_threadsRepository.find();
+  }
+
+  async findAllReporteds(): Promise<Reportment_comment[]> {
+    return this.reportment_commentsRepository.find();
+  }
+}
