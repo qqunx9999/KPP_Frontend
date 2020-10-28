98ffa7a51142e11c6bb87de65299994b02904b29
athitapat <e4wearth@gmail.com>
2020-10-28 11:46:34 +0700

    change struct thread with own

--- backend/src/threads/threads.controller.ts
+++ backend/src/threads/threads.controller.ts
@@ -57,6 +57,12 @@ export class ThreadsController {
     return {threads, pageInfo:{pagesize: threads.length, pageNo, total: totals}};
   }
 
+  @Get( ':threadID/comments/:commentID')
+  async findOneComment(@Param('threadID', ParseObjectIdPipe) threadID: ObjectID,
+    @Param('commentID', ParseObjectIdPipe) commentID: ObjectID,
+  ): Promise<any>{
+    return this.threadsService.findOneComment(commentID);
+  }
   
 
   @Get(':threadID')
