ea6cc07f4ab6cbce73ecc2859872772100f3cae2
pchya <phitchaya.le@ku.th>
2020-10-31 18:08:05 +0700

    Fix report.module

--- backend/src/reports/reports.module.ts
+++ backend/src/reports/reports.module.ts
@@ -5,6 +5,7 @@ import Reportment_comment from 'src/entities/reportment_comment.entity';
 import Reportment_thread from 'src/entities/reportment_thread.entity';
 import Commentation from 'src/threads/comentation.entity';
 import Thread from 'src/threads/thread.entity';
+import Admin from 'src/entities/admin.entity';
 
 import { ThreadsModule } from 'src/threads/threads.module';
 
@@ -13,7 +14,7 @@ import { ReportsService } from './reports.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Thread, Commentation, Reportment_thread,Reportment_comment]),
+    TypeOrmModule.forFeature([Thread, Commentation, Reportment_thread,Reportment_comment, Admin]),
     ThreadsModule
   ],
   controllers: [ReportsController],
