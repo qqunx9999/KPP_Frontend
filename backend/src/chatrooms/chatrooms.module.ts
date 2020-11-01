04c191d49c9c8f2aff5b7812292a8b1468cae244
athitapat <e4wearth@gmail.com>
2020-11-01 22:27:38 +0700

    notification friend and chat

--- backend/src/chatrooms/chatrooms.module.ts
+++ backend/src/chatrooms/chatrooms.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Chatroom from 'src/entities/chatroom.entity';
 import Chat_message from 'src/entities/chat_message.entity';
+import { NotificationModule } from 'src/notification/notification.module';
 import { UsersModule } from 'src/users/users.module';
 import { ChatroomsController } from './chatrooms.controller';
 import { ChatroomsService } from './chatrooms.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Chatroom, Chat_message]),
-      UsersModule
+      UsersModule,
+      NotificationModule
   ],
   controllers: [ChatroomsController],
   providers: [ChatroomsService]
--- backend/src/notification/notification.controller.ts
+++ backend/src/notification/notification.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Delete, Param, Body, Patch } from '@nestjs/common';
 import { ObjectID } from 'mongodb'
 import { ParseObjectIdPipe } from 'src/common/pipes';
-import { NotificationService } from './notification.service';
+import { NotificationsService } from './notification.service';
 import { Notifications } from 'src/entities/notification.entity';
 import { NotificationDto } from 'src/dto/create-notifiaction.dto';
 import Chatroom from 'src/entities/chatroom.entity';
@@ -9,7 +9,7 @@ import Chatroom from 'src/entities/chatroom.entity';
 @Controller('notifications')
 export class NotificationController {
 
-    constructor(private notificationService: NotificationService) {}
+    constructor(private notificationService: NotificationsService) {}
 
     @Get('/contacts/users/:userID')
     async allUnread(@Param('userID', ParseObjectIdPipe) userID: ObjectID): Promise <Notifications[]> {
@@ -23,50 +23,50 @@ export class NotificationController {
         return {friend_noti,report_noti}
     }
 
-    @Post('/friend_request')
-    async postFriendRequest(@Body() notificationDto: NotificationDto){
-        notificationDto.userID = new ObjectID(notificationDto.userID);
-        notificationDto.object_type = "friend_request";
-        notificationDto.object_typeID = new ObjectID(notificationDto.object_typeID);
-        return this.notificationService.postFriendRequest(notificationDto);
-    }
+    // @Post('/friend_request')
+    // async postFriendRequest(@Body() notificationDto: NotificationDto){
+    //     notificationDto.userID = new ObjectID(notificationDto.userID);
+    //     notificationDto.object_type = "friend_request";
+    //     notificationDto.object_typeID = new ObjectID(notificationDto.object_typeID);
+    //     return this.notificationService.postFriendRequest(notificationDto);
+    // }
 
-    @Post('/friend_accepted')
-    async postAcceptFriend(@Body() notificationDto: NotificationDto){
-        notificationDto.userID = new ObjectID(notificationDto.userID);
-        notificationDto.object_type = "friend_accepted";
-        notificationDto.object_typeID = new ObjectID(notificationDto.object_typeID); 
-        return this.notificationService.postAcceptFriend(notificationDto);
-    }
+    // @Post('/friend_accepted/')
+    // async postAcceptFriend(@Body() notificationDto: NotificationDto){
+    //     notificationDto.userID = new ObjectID(notificationDto.userID);
+    //     notificationDto.object_type = "friend_accepted";
+    //     notificationDto.object_typeID = new ObjectID(notificationDto.object_typeID); 
+    //     return this.notificationService.postAcceptFriend(notificationDto);
+    // }
 
-    @Post('/chatrooms/:chatroomID/users/:userID')
-    async postChatrooms(@Param('chatroomID', ParseObjectIdPipe) chatroomID: ObjectID,
-        @Param('userID', ParseObjectIdPipe) userID: ObjectID,
-        @Body() notificationDto: NotificationDto){
-        return this.notificationService.postChatroom(chatroomID, userID);
-    }
+    // @Post('/chatrooms/:chatroomID/users/:userID')
+    // async postChatrooms(@Param('chatroomID', ParseObjectIdPipe) chatroomID: ObjectID,
+    //     @Param('userID', ParseObjectIdPipe) userID: ObjectID,
+    //     @Body() notificationDto: NotificationDto){
+    //     return this.notificationService.postChatroom(chatroomID, userID);
+    // }
 
-    @Post('/reports')
-    async postReport(@Body() notificationDto: NotificationDto){
-        notificationDto.userID = new ObjectID(notificationDto.userID);
-        notificationDto.object_type = "report";
-        notificationDto.object_typeID = null;
-        notificationDto.date_noti = new Date();
-        notificationDto.date_read = null;
-        return this.notificationService.findReport(notificationDto);
-    }
+    // @Post('/reports')
+    // async postReport(@Body() notificationDto: NotificationDto){
+    //     notificationDto.userID = new ObjectID(notificationDto.userID);
+    //     notificationDto.object_type = "report";
+    //     notificationDto.object_typeID = null;
+    //     notificationDto.date_noti = new Date();
+    //     notificationDto.date_read = null;
+    //     return this.notificationService.findReport(notificationDto);
+    // }
 
-    @Post('/report_considered')
-    async postReportCon(@Param('chatroomID', ParseObjectIdPipe) chatroomID: ObjectID, 
-        @Body() notificationDto: NotificationDto){
-        notificationDto.userID = new ObjectID(notificationDto.userID);
-        notificationDto.object_type = "report_considered";
-        notificationDto.object_typeID = chatroomID;
-        notificationDto.date_read = null;
-        return this.notificationService.postReportCon(notificationDto);
-    }
+    // @Post('/report_considered')
+    // async postReportCon(@Param('chatroomID', ParseObjectIdPipe) chatroomID: ObjectID, 
+    //     @Body() notificationDto: NotificationDto){
+    //     notificationDto.userID = new ObjectID(notificationDto.userID);
+    //     notificationDto.object_type = "report_considered";
+    //     notificationDto.object_typeID = chatroomID;
+    //     notificationDto.date_read = null;
+    //     return this.notificationService.postReportCon(notificationDto);
+    // }
 
-    @Patch('/chatrooms/:chatroomID/users/:userID-')
+    @Patch('/chatrooms/:chatroomID/users/:userID')
     async patchChatroom(@Param('chatroomID', ParseObjectIdPipe) chatroomID: ObjectID,
     @Param('userID', ParseObjectIdPipe) userID: ObjectID): Promise <Notifications>{
         return this.notificationService.patchChatroom(userID, chatroomID);
