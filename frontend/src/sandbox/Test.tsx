f348d0a9c71e0237d6fdd3c7db633008721dbd2f
ppjaisri <ppjaisri@gmail.com>
2020-11-22 14:14:41 +0700

    working list update

--- frontend/src/sandbox/Test.tsx
+++ frontend/src/sandbox/Test.tsx
@@ -2,80 +2,76 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.module.css';
 import AuthService from '../service/AuthService';
-import SearchThreadForm from '../component/SearchThreadForm';
+import ReadReport from '../Pages/ReadReport';
 
-// export default class Test extends React.Component {
-//   state: any = {
-//     threads: [],
-//     threadsInfo: [],
-//     comment: [],
-//     commentInfo: [],
-//     thread: [],
-//     threadInfo: []
-//   }
+export default class Test extends React.Component {
+  state: any = {
+    threads: [[{}]],
+    threadsInfo: [],
+    comment: [],
+    commentInfo: [],
+    thread: [],
+    threadInfo: []
+  }
 
-//   fetchThread = async (userID: string, pageNo: number) => {
-//     let response = await fetch(`http://localhost:3000/reports/reportTs/list/${userID}/8/${pageNo}`, {
-//       method: 'GET',
-//       headers: {
-//         'Accept': 'applicaiton/json',
-//         'Content-Type': 'application/json'
-//       }
-//     })
+  fetchThread = async (userID: string, pageNo: number) => {
+    let response = await fetch(`http://localhost:3000/reports/reportTs/list/${userID}/8/${pageNo}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'applicaiton/json',
+        'Content-Type': 'application/json'
+      }
+    })
 
-//     const data = response.json().then(res => this.setState({
-//       threads: res.RTs_page,
-//     }));
-//   }
+    const data = response.json().then(res => this.setState({
+      threads: res.RTs_page,
+    }));
+  }
 
-//   fetchOneThread = async (reportID: string) => {
-//     let response = await fetch(`/reports/reportTs/${ reportID }`, {
-//       method: 'GET',
-//       headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//       }
-//     })
+  fetchOneThread = async (reportID: string) => {
+    let response = await fetch(`/reports/reportTs/${ reportID }`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
 
-//     const data = response.json().then(res => this.setState({
-//       thread: res[1],
-//       threadInfo: res[2]
-//     }));
-//     // console.dir(data);
-//   }
+    const data = response.json().then(res => this.setState({
+      thread: res[1],
+      threadInfo: res[2]
+    }));
+    // console.dir(data);
+  }
 
-//   componentDidMount() {
-//     const userID = AuthService.getUserID();
-//     this.fetchThread(userID, 1);
-//     // this.fetchOneThread();
-//   }
+  componentDidMount() {
+    const userID = AuthService.getUserID();
+    this.fetchThread(userID, 1);
+    // this.fetchOneThread();
+  }
 
-//   render() {
-//     console.dir(this.state.threads);
-//     let report: any = [];
+  render() {
+    console.dir(this.state.threads[0][0]);
+    let report: any = [];
 
-//     if (this.state.threads !== []) {
-//       report = this.state.threads.map((thread: any) => (
-//         <li>
-//           <h1>Report Thread</h1>
-//           { thread.reportID }
-//         </li>
-//       ))
-//     }
-//     return (
-//       <div>
-//         { report }
-//       </div>
-//     );
-//   }
-// }
+    function handleReadThread(reportTID: string) {
+      return (
+        <ReadReport />
+      );
+    }
 
-const temp = { "margin": "10px" }
-
-export default function Test() {
-  return (
-    <div style={ temp }>
-      <SearchThreadForm />
-    </div>
-  );
+    if (this.state.threads !== []) {
+      report = this.state.threads.map((thread: any) => (
+        <li>
+          <h1>Report Thread</h1>
+          <button onClick={ () => handleReadThread(thread[0].reportTID) }>read</button>
+        </li>
+      ))
+    }
+    return (
+      <div>
+        { report }
+      </div>
+    );
+  }
 }
